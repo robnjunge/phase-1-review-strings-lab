e596e5b83cd97f75fcaf2e9491d947580b68ff88
robertnjunge <mubearobert@gmail.com>
2023-07-12 16:27:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Robert Njunge";//defines the currentUser variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;//defines the welcomeMessage and adds an exclamation mark and the value of the currentUser variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;//defines the excitedWelcomeMessage and adds an exclamation mark and the value of the currentUser variable toUpperCase
+const shortGreeting = `Welcome, ${currentUser[0]}!`;//defines the shortGreeting and adds an exclamation mark and the value of the currentUser first initial variable
